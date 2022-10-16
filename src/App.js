6eee4606f995766a89604dd8e77079be92b86d08
ishilbi <107364357+ishilbi@users.noreply.github.com>
2022-10-16 12:40:41 -0400

    Added footer link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Weather from "./Weather";
+import Footer from "./Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Weather />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <p class="text-center mt-5">
+      This project was coded by{" "}
+      <a href="mailto:silvialeelee7@gmail.com" class="footer-link">
+        Silvia Lee
+      </a>{" "}
+      and is{" "}
+      <a
+        href="https://github.com/ishilbi/portfolio-project"
+        target="_blank"
+        rel="noreferrer"
+        class="footer-link"
+      >
+        open-sourced on GitHub
+      </a>{" "}
+      and hosted on{" "}
+      <a
+        href="https://www.netlify.com/"
+        target="_blank"
+        rel="noreferrer"
+        class="footer-link"
+      >
+        Netlify
+      </a>
+    </p>
+  );
+}
