f0de5552513e5798ba9f28a7daa52e84c2d6bc62
ishilbi <107364357+ishilbi@users.noreply.github.com>
2022-10-16 12:23:57 -0400

    removed image

--- src/Weather.js
+++ src/Weather.js
@@ -14,9 +14,7 @@ export default function Weather() {
   return (
     <div className="weather-app">
       <div className="overview">
-        <div className="weather-title">
-          Weather <img src="image/weather.svg" alt="headerImage" width="90px" />
-        </div>
+        <div className="weather-title">Weather</div>
         <form>
           <div className="row">
             <div class="col-9">
