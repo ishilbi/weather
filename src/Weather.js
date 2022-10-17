a0bb6aa79ada8e6815aa31c7ea2088163ceca37f
ishilbi <107364357+ishilbi@users.noreply.github.com>
2022-10-17 16:52:17 -0400

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,75 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Seoul",
-    temperature: 30,
-    date: "Wednesday 14, 2022; 10:00",
-    description: "Sunny",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-    humidity: 80,
-    wind: 10,
-  };
-  return (
-    <div className="Weather">
-      <div className="weather-app">
-        <div className="overview">
-          <div className="weather-title">
-            Weather
-            <img src="{image/weather.svg}" alt=""></img>
-          </div>
-          <form>
-            <div className="row">
-              <div className="col-9">
-                <input
-                  type="search"
-                  placeholder="Enter a city..."
-                  className="form-control search-input"
-                  autoComplete="off"
-                />
-              </div>
-              <div className="col-3 p-0">
-                <input
-                  type="submit"
-                  className="btn btn-primary w-100"
-                  value="Search"
-                />
-              </div>
-            </div>
-          </form>
-        </div>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-        <div className="weather-info">
-          <div className="row">
-            <div className="col-6">
-              <h1>{weatherData.city}</h1>
-              <h2>{weatherData.date}</h2>
-              <ul>
-                <li>{weatherData.description}</li>
-                <li>Humidity: {weatherData.humidity}%</li>
-                <li>Wind Speed: {weatherData.wind}km/hr</li>
-              </ul>
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      date: "Mon Oct 17, 2022 15:46pm",
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      tempMax: response.data.main.temp_max,
+      tempMin: response.data.main.temp_min,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="weather-app">
+          <div className="overview">
+            <div className="weather-title">
+              Weather
+              <img src="{image/weather.svg}" alt=""></img>
             </div>
-            <div className="col-6">
-              <div className="clearfix weather-temperature">
-                <div className="float-right">
-                  <span className="temperature">{weatherData.temperature}</span>
-                  <span className="units">
-                    <a href="/">°C</a> | <a href="/">°F</a>
-                  </span>
+            <form>
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Enter a city..."
+                    className="form-control search-input"
+                    autoComplete="off"
+                  />
                 </div>
-                <img
-                  src={weatherData.imgUrl}
-                  alt={weatherData.description}
-                  className="float-right"
-                />
+                <div className="col-3 p-0">
+                  <input
+                    type="submit"
+                    className="btn btn-primary w-100"
+                    value="Search"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div className="weather-info">
+            <div className="row">
+              <div className="col-6">
+                <h1>{weatherData.city}</h1>
+                <h2>{weatherData.date}</h2>
+                <ul>
+                  <li className="text-capitalize">{weatherData.description}</li>
+                  <li>Temp min: {Math.round(weatherData.tempMin)}°C</li>
+                  <li>Temp max: {Math.round(weatherData.tempMax)}°C</li>
+                  <li>Humidity: {weatherData.humidity}%</li>
+                  <li>Wind: {weatherData.wind} km/hr</li>
+                </ul>
+              </div>
+              <div className="col-6">
+                <div className="clearfix weather-temperature">
+                  <div className="float-right">
+                    <span className="temperature">
+                      {Math.round(weatherData.temperature)}
+                    </span>
+                    <span className="units">
+                      <a href="/">°C</a> | <a href="/">°F</a>
+                    </span>
+                  </div>
+                  <img
+                    src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+                    alt="sunnycloudy"
+                  ></img>{" "}
+                </div>
               </div>
             </div>
           </div>
+          <div className="weather-forecast"></div>
         </div>
-        <div className="weather-forecast"></div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "f3a66372ec0940e078d8212f937f2ff1";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading..";
+  }
 }
