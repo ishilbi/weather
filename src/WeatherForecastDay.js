13914b9948bb594557ee7037795b97eefcb27fff
ishilbi <107364357+ishilbi@users.noreply.github.com>
2022-10-23 20:36:34 -0400

    built a 5 day forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -13,13 +13,16 @@ export default function WeatherForecastDay(props) {
   function day() {
     let date = new Date(props.data.dt * 1000);
     let day = date.getDay();
+
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    return days(day);
+
+    return days[day];
   }
+
   return (
     <div>
       {" "}
-      <div className="WeatherForecast-days">{day()}</div>
+      <div className="WeatherForecast-day">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
